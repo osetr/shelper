91e531fd6bec7420ff0620c86e58cdc7714bd1d4
anton <uawesome13@gmail.com>
2020-07-29 18:40:20 +0300

    migration added

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:uawesome120300@127.0.0.1:3306/shelper'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['CHARSET'] = 'utf8'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
